d45d9e7a577fac89d00a5c96cc206bc5ee9b9301
Raphael Kuster <raphael.kuster@mia-systems.at>
2018-12-19 11:09:59 +0100

    Init and test with https://app.quicktype.io

---
+++ MadMapperRemote.Console/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using MadMapperRemote.Core;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace MadMapperRemote.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var structure = MadMapperAccess.GetStructure("http://localhost:8010");
+			
+		}
+	}
+}
